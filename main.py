f72acf9690694bd73c75968af49b5481b5c33027
HitmanTheGreat <79275310+HitmanTheGreat@users.noreply.github.com>
2022-04-10 13:21:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import base64
 page_bg_img = '''
 <style>
 body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-image: url(os.path.join('static/images/background.jpg'));
 background-size: cover;
 }
 </style>
