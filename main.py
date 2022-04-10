963776d9fac332dc9b57bd57a8d78bac7a572f68
HitmanTheGreat <79275310+HitmanTheGreat@users.noreply.github.com>
2022-04-10 13:31:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def set_png_as_page_bg(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
 
-set_png_as_page_bg('background.png')
+set_png_as_page_bg(os.path.join('static/images/background.jpg'))
 st.markdown("<center><h1 style='text-shadow:3px 3px 4px blue'><b><u>Object Detection Machine Learning</u> </b></h1></center>",unsafe_allow_html=True)
 #converting image into array
 def load_image(image_path):
