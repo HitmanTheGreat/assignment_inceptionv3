c096f879425dbcf0d5f8ea516871f85e4b875f26
HitmanTheGreat <79275310+HitmanTheGreat@users.noreply.github.com>
2022-04-10 10:01:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ import PIL
 import time
 import streamlit as st
 import base64
-
-st.markdown("<center><h1 style='color:blue'><b>Object Detection Machine Learning </b></h1></center>",unsafe_allow_html=True)
+#header
+st.markdown("<center><h1 style='text-shadow:3px 3px 4px blue'><b><u>Object Detection Machine Learning</u> </b></h1></center>",unsafe_allow_html=True)
 #converting image into array
 def load_image(image_path):
-    img = Image.open( image_path )
+    img = Image.open(image_path )
     newImg = img.resize((299,299), PIL.Image.BILINEAR).convert("RGB")
     data = np.array( newImg.getdata() )
     return 2*( data.reshape( (newImg.size[0], newImg.size[1], 3) ).astype( np.float32 )/255 ) - 1
@@ -29,6 +29,13 @@ def predictor(img_path): #
     image = np.expand_dims(image, axis=0)
     preds = model.predict(image)
     return(preds)
+#deleing  video to save space on the server
+def delete_uploaded_file():
+    try:
+        os.remove(videopath)
+        return 1
+    except:
+        return 0
 #saving video
 def save_uploaded_file(uploaded_file,type=["mp4","avi","webm","mov",""]):
     try:
@@ -39,11 +46,12 @@ def save_uploaded_file(uploaded_file,type=["mp4","avi","webm","mov",""]):
         return 0
 #put queries here
 search = st.text_input("Type The Name Of Search Object....")
+#setting upload file whith required video types
 uploaded_file = st.file_uploader("Upload Video",type=["mkv","mp4",'avi','flv','webm','vob','mov','ogg','wmv','3gp'])
+#handling uploads
 if uploaded_file is not None:
-    notfound = True
-    img = Image.open(os.path.join('static/images/loading.gif'))
-    if save_uploaded_file(uploaded_file):   	
+    notfound = True # search flag
+    if save_uploaded_file(uploaded_file):#handling saving and video proccessing
         capture = cv2.VideoCapture(os.path.join('static/videos',uploaded_file.name))
         global videopath 
         videopath = os.path.join('static/videos',uploaded_file.name)
@@ -54,7 +62,7 @@ if uploaded_file is not None:
         file_.close()
 
         st.markdown(
-            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+            f'<marquee><img src="data:image/gif;base64,{data_url}" alt="cat gif"></marquee>',
             unsafe_allow_html=True,
         )
         frameNr = 0
@@ -76,7 +84,6 @@ if uploaded_file is not None:
                     #display prediction text over the image
                     pic = cv2.putText(disp_img, search + " Is On this Frame", (40,40), cv2.FONT_HERSHEY_TRIPLEX , 1.8, (0,0,0))
                     st.image(pic)
-                    st.text('Predictions :-')
                     st.success("Other Items Detected : " + str(predition_names))
                     os.remove(os.path.join(f'static/images/{frameNr}.jpg'))#deleting the image to savespace 
                     break
@@ -88,7 +95,6 @@ if uploaded_file is not None:
         capture.release()
         time.sleep(20) # delay b4 deleting the video
         if notfound :
-            st.write("Item you searched was not found ")
-        st.warning("Code Executed Successfully !!!!!!!!!!")
-    os.remove(videopath)
-
+            st.warning("Item you searched was not found ")
+        st.success("Code Executed Successfully !!!!!!!!!!")
+        delete_uploaded_file()
